214ba55a2b067d484780ad20644d78083a93d31b
FaCoInf <corinne.fagno@gmail.com>
2014-05-29 18:49:18 +0200

    Tests Serveur Reception

--- ServeurWeb/src/serveurweb/Serveur/Communication.java
+++ ServeurWeb/src/serveurweb/Serveur/Communication.java
@@ -6,12 +6,13 @@
 package serveurweb.Serveur;
 
 import java.io.BufferedInputStream;
-import java.io.DataInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,7 +23,8 @@ import java.util.logging.Logger;
 public class Communication extends Thread {
 
     private Socket socket;
-    //private ServerSocket socketServeur;
+    private BufferedOutputStream outputStream;
+    private BufferedInputStream inputStream;
 
     public Communication(Socket connexion) {
         socket = connexion;
@@ -30,32 +32,28 @@ public class Communication extends Thread {
 
     @Override
     public void run() {
-        super.run();
 
         //Information
-        System.err.println("Connecté : " + socket.toString());
+        System.out.println("Connecté : " + socket.toString());
 
         //    try {
         while (true) {// tant que connection non close
             try {
                 //Recuperation de la demande
-                InputStream is = socket.getInputStream();
+                /////////////////////////////////////////////////////////         InputStream is = socket.getInputStream();
                 //BufferedInputStream buff = new BufferedInputStream(is);
                 //DataInputStream dis = new DataInputStream(is);
-                
+
          //       message = is.read();
-                
-                
-                
                 /*while (buff.available() != 0) {
-				System.out.println(buff.read());
-			}*/
+                 System.out.println(buff.read());
+                 }*/
                 /*String message = null;*/
                 // read until a single byte is available
                 //while (in.available() !=-1) {
                 // read the byte and convert the integer to character
                 //char c = (char) in.read();
-                    // print the characters
+                // print the characters
                 //  System.out.println("Char: " + c);
                 //message = message + c;
                 //}
@@ -68,19 +66,18 @@ public class Communication extends Thread {
                  String st = new String(by, 0, j);
                  System.out.print(st);
                  }*/
-                StringBuffer response = new StringBuffer();
-  //  try {
+    ///////////////////////////////////////            StringBuffer response = new StringBuffer();
+                //  try {
                 //BufferedInputStream buff = new BufferedInputStream(System.in);
-                
-       /*          int in = 0;
-                char inChar;
-                while(in!= )
+                /*          int in = 0;
+                 char inChar;
+                 while(in!= )
               
                 
-                    in = buff.readline();
-                    inChar = (char) in;
-                //buff.close();
-                System.out.println("Message: " + response.toString());*/
+                 in = buff.readline();
+                 inChar = (char) in;
+                 //buff.close();
+                 System.out.println("Message: " + response.toString());*/
                 /* } catch (IOException e) {
                  System.out.println("Exception: " + e.getMessage());
                  }*/
@@ -88,10 +85,65 @@ public class Communication extends Thread {
                 /*  } catch (IOException ex) {
                  Logger.getLogger(Communication.class.getName()).log(Level.SEVERE, null, ex);
                  }*/
-       //     }
+                //     }
             } catch (Exception ex) {
                 Logger.getLogger(Communication.class.getName()).log(Level.SEVERE, null, ex);
             }
+
+            while (true) {
+                try {
+                    OutputStream o = socket.getOutputStream();
+                    outputStream = new BufferedOutputStream(o);
+                    InputStream i = socket.getInputStream();
+                    inputStream = new BufferedInputStream(i);
+             
+
+                Byte[] recu = RecevoirData();
+                String s = recu.toString();
+                System.out.println(s);
+                
+                   } catch (Exception e) {
+                    System.err.println("piou" + e.getMessage());
+                }
+            }
+        }
+    }
+
+    private Byte[] RecevoirData() {
+        ArrayList<Byte> recu = new ArrayList<>();
+        byte temp;
+        try {
+            while ((temp = (byte) inputStream.read()) != -1) {
+                recu.add(temp);
+            }
+        } catch (SocketTimeoutException e) {
+            System.err.println("le serveur ne répond pas" + e.getMessage());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+
+        return (Byte[]) recu.toArray();
+
+    }
+
+    private void envoyerReq() {
+        String data = "GET http://www.univ-lyon1.fr/test.html";
+        System.out.println("sur le point d'envoyer");
+        try {
+            outputStream.write(data.getBytes());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+        System.out.println("envoyé");
+    }
+
+    private void fermerConnexion() {
+        try {
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
