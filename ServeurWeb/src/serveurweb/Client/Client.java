33de69d7aff10451ef3a53bf45a5af152299cf19
premillieu.laura@gmail.com <>
2014-05-30 17:41:08 +0200

    interface graphique + gestion champs non remplis, serveur injoignable, serveur ne répond pas

---
+++ ServeurWeb/src/serveurweb/Client/Client.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serveurweb.Client;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Client {
+
+    private Socket socket;
+    private InetAddress server;
+    private int port;
+    private BufferedOutputStream out;
+    private BufferedInputStream in;
+
+    public Client(InetAddress s, int p) {
+        try {
+            server = s;
+            port = p;
+
+        } catch (Exception e) {
+            System.err.println("piou" + e.getMessage());
+        }
+    }
+
+    /**
+     * Permet d'initialiser le socket et la connexion
+     *
+     * @return 0 si tout s'est bien passé 1 s'il y a eu un problème de connexion
+     * avec le serveur 2 IO exception
+     */
+    public int initSocket() {
+        try {
+            socket = new Socket(server, port);
+            socket.setSoTimeout(1000);
+            OutputStream o = socket.getOutputStream();
+            out = new BufferedOutputStream(o);
+            InputStream i = socket.getInputStream();
+            in = new BufferedInputStream(i);
+        } catch (SocketException ex) {
+            System.err.println("pouet  " + ex.getMessage());
+
+            return 1;
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            return 2;
+        }
+        return 0;
+    }
+
+    /**
+     * 
+     * @return  1 = timeout 2 = IOexception 0 = ok
+     */
+    public int run() {
+        Byte[] recu;
+        envoyerReqGet("http://www.univ-lyon1.fr/test.html");
+        recu = RecevoirData();
+
+        if (recu.length == 1) {
+            return recu[0];
+        }
+        String s = recu.toString();
+        System.out.println(s);
+        fermerConnexion();
+        return 0;
+    }
+
+    private void envoyerReqGet(String site) {
+        String data = "GET " + site + " HTTP/1.1";
+
+        System.out.println("sur le point d'envoyer");
+        try {
+            out.write(data.getBytes());
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+        System.out.println("envoyé");
+    }
+
+    /**
+     *
+     * @return 1 = timeout 2 = IOexception le tableau de byte recu si ok
+     */
+    private Byte[] RecevoirData() {
+        ArrayList<Byte> recu = new ArrayList<>();
+        byte temp;
+        try {
+            while ((temp = (byte) in.read()) != -1) {
+                recu.add(temp);
+            }
+        } catch (SocketTimeoutException e) {
+            System.err.println("le serveur ne répond pas " + e.getMessage());
+            Byte[] b = new Byte[1];
+            b[0] = 1;
+            return b;
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            Byte[] b = new Byte[1];
+            b[0] = 2;
+            return b;
+        }
+
+        return (Byte[]) recu.toArray();
+
+    }
+
+    private void fermerConnexion() {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
--- ServeurWeb/src/serveurweb/Client/MainClient.java
+++ ServeurWeb/src/serveurweb/Client/MainClient.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package serveurweb;
+package serveurweb.Client;
 
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -16,17 +16,14 @@ public class MainClient {
         Client c;
         try 
         {
-            InetAddress server =  InetAddress.getByName("169.254.182.165");
+            InetAddress server =  InetAddress.getByName("127.0.0.1");
             int port = 80;
             c = new Client(server,port);
             c.run();
         } catch (UnknownHostException ex) 
         {
             System.err.println(ex.getMessage());
-        } catch (SocketException ex)
-        {
-            System.err.println(ex.getMessage());
-        }
+        } 
         
         
     }
